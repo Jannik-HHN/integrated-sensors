344ccbcdd74dc4f747b613a540ca4507a0c285b2
Jannik-HHN <maplejannik3@web.de>
2022-01-08 16:07:45 +0100

    Added more comments

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,6 @@ public class Main {
     int dataInMs = 360;
 
     try {
-
       System.out.println("-----------------------------");
       // Open the wav file specified as the first argument
       WavFile wavFile = WavFile.openWavFile(new File("res/HackRF-110054-Mono.wav"));
@@ -36,35 +35,41 @@ public class Main {
       framesRead = wavFile.readFrames(bufferCarrier, samplesPerMs * carrierInMs);
       framesRead = wavFile.readFrames(bufferData, samplesPerMs * dataInMs);
 
-      // Create a 10ms carrier instance that loops perfectly
-      int[] bufferCarrierInstance = Arrays.copyOfRange(bufferCarrier, bufferCarrier.length - (10 * samplesPerMs + 60000), bufferCarrier.length - 60000);
+      // Create a 10ms carrier frequency buffer that loops perfectly
+      int[] bufferCarrierInstance = Arrays.copyOfRange(bufferCarrier, bufferCarrier.length - (10 * samplesPerMs + 1835), bufferCarrier.length - 1835);
       // int[] bufferCarrierInstance = Arrays.copyOfRange(bufferCarrier, 0, 20000);
 
-      int bufferCounter = 0;
-      double[] totalBlockDiff = new double[2];
-      String bitString = "";
-      int bits = (samplesPerMs * dataInMs) / 5000;
-      int totalBits = 0;
+      String bitString = "";                        // The final Bit String
+      int bits = (samplesPerMs * dataInMs) / 5000;  // Total Bits of Message regarding Sample Rate
+      int totalBits = 0;                            // Current amount of Bits
+      int bufferCounter = 0;                        // Current position inside the buffer
+      double[] totalBlockDiff = new double[2];      // Left (0) and Right (1) side of a Bit compared
+
       do {
         totalBlockDiff[0] = 0;
         totalBlockDiff[1] = 0;
         totalBits++;
 
-        // Loop through frames and look for minimum and maximum value
+        // Loop through samples and compare Data Sample with Carrier Sample
         for (int s = (totalBits-1) * 5000; s < totalBits * 5000; s++) {
-          int blockDiff = 0;
+          int blockDiff = 0;    // Reset Difference
+
+          // Calculate the absolute Difference of Data and Carrier Frequency Sample
           blockDiff = Math.max(bufferData[s], bufferCarrierInstance[bufferCounter % bufferCarrierInstance.length]) - Math.min(bufferData[s], bufferCarrierInstance[bufferCounter % bufferCarrierInstance.length]);
-          //System.out.println((blockDiff));
-          //blockDiff = Math.abs(bufferData[s]) - Math.abs(bufferCarrierInstance[bufferCounter % bufferCarrierInstance.length]);
+
+          // Add the absolute Difference to Left Side of the Bit if below first half
           if (s%5000 < (samplesPerBit / 2)) {
             totalBlockDiff[0] += (double) blockDiff * 0.00001;
-            // if (blockDiff < 1500) totalBlockDiff[0]++;
-          } else {
-            totalBlockDiff[1] += (double) blockDiff * 0.00001;
-            // if (blockDiff < 1500) totalBlockDiff[1]++;
           }
-          bufferCounter++;
+          // Add the absolute Difference to Right Side of the Bit if above first half
+          else {
+            totalBlockDiff[1] += (double) blockDiff * 0.00001;
+          }
+
+          bufferCounter++;      // Increase Position of Buffer after each read sample
         }
+
+        // Print out information and append Bit to String
         System.out.print("  Bit " + totalBits + ":\t" + df.format(totalBlockDiff[0]) + "\t\t" + df.format(totalBlockDiff[1]));
         System.out.println("\t|\t" + (totalBlockDiff[0] > totalBlockDiff[1] ? 1 : 0));
         bitString += (totalBits%4) == 1 ? " ": "";
@@ -72,12 +77,10 @@ public class Main {
       }
       while (totalBits < bits);
 
-      System.out.println(bitString);
-
-      // Close the wavFile
-      wavFile.close();
-
-    } catch (Exception e) {
+      System.out.println(bitString);    // Print out complete Bit String
+      wavFile.close();                  // Close the Wav File
+    }
+    catch (Exception e) {
       System.err.println(e);
     }
   }
